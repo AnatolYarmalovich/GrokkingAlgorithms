c4408807f14455cb701a4f127cf6f492abc985fb
AnatolYarmolovich <yarmolovich1986@gmail.com>
2022-06-11 23:59:06 +0200

    initial commit

---
+++ GrokkingAlgorithms.playground/Contents.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+//Grokking Algorithms by Aditya Y. Bhargava
+//Swift solutions by Yarmalovich Anatol
+
+/// Chapter 1
+//Binary sort algorithm
+
+var binarySearchMockList = [1,2,3,4,5,6,7,8,9,10,11,12]
+
+func binarySearch
