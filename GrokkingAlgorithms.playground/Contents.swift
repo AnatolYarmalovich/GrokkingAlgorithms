b3c9e91223979bc2afd6cc0f8f9e9c6c4a3bbe0f
AnatolYarmolovich <yarmolovich1986@gmail.com>
2022-06-12 23:10:16 +0200

    Selection sort algorithm added

--- GrokkingAlgorithms.playground/Contents.swift
+++ GrokkingAlgorithms.playground/Contents.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 //Grokking Algorithms by Aditya Y. Bhargava
-//Swift solutions by Yarmalovich Anatol
+//Swift algorithms version by Yarmalovich Anatol
 
 /// Chapter 1
 //Binary sort algorithm
@@ -40,3 +40,35 @@ let resultToConsole = resultOfBinarySearch != nil
 
 print(resultToConsole)
 
+/// Chapter 2
+//Selection sort algorithm
+var unsortedMockArray: [Int] = [4, 2, 5, 7, 1, 34, 21, 9]
+
+//Part one - search smallest value index
+func findSmallest(array: [Int]) -> Int {
+    var smallestValue = array[0]
+    var smallestIndex = 0
+
+    for i in 1..<array.count {
+        if array[i] < smallestValue {
+            smallestValue = array[i]
+            smallestIndex = i
+        }
+    }
+    return smallestIndex
+}
+
+//Part two - selection sort algorithm
+func selectionSort(array: [Int]) -> [Int] {
+    var sortedArray = [Int]()
+    var tempArray = array
+
+    for _ in array {
+        let smallest = findSmallest(array: tempArray)
+        sortedArray.append(tempArray.remove(at: smallest))
+    }
+    return sortedArray
+}
+
+let selectionSortResult = selectionSort(array: unsortedMockArray)
+print("Selection sort result: \(selectionSortResult)")
